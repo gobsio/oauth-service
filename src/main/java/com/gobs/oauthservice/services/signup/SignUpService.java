f0ac90069644c4defc3914524c4a10bfbae17614
Lucas Martins <dev.lucasmartins@gmail.com>
2020-04-12 20:50:12 -0300

    added users controller

--- src/main/java/com/gobs/oauthservice/services/signup/SignUpService.java
+++ src/main/java/com/gobs/oauthservice/services/signup/SignUpService.java
@@ -1,23 +1,42 @@
 package com.gobs.oauthservice.services.signup;
 
+import javax.inject.Inject;
+
 import com.gobs.oauthservice.domain.dtos.UserDTO;
 import com.gobs.oauthservice.domain.entities.User;
 import com.gobs.oauthservice.domain.mappers.users.UserMapper;
+import com.gobs.oauthservice.repositories.users.UsersRepository;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpService {
 
+    @Inject
+    UsersRepository usersRepository;
+
     public UserDTO register(UserDTO userDTO) throws Exception {
-        // transform DTO to Entity
         User user = UserMapper.toEntity(userDTO);
 
-        // validating...
+        validate(user);
 
-        // saving...
-        
-        return UserMapper.toDto(user); // transform to dto.
+        return UserMapper.toDto(usersRepository.save(user)); 
+    }
+
+    private boolean validate(User user) throws Exception {
+        if (user.getFirstName() == null || user.getFirstName().equals("")) {
+            throw new IllegalArgumentException("");
+        }
+        if (user.getEmail() == null || user.getEmail().equals("")) {
+            throw new IllegalArgumentException("");
+        }
+        if (user.getPhone() == null || user.getPhone().equals("")) {
+            throw new IllegalArgumentException("");
+        }
+        if (user.getPassword() == null || user.getPassword().equals("")) {
+            throw new IllegalArgumentException("");
+        }
+        return true;
     }
 
 }
