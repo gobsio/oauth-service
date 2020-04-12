7b0ee4f229cc3603d8e008dfd12f2c68bc57a589
Lucas Martins <dev.lucasmartins@gmail.com>
2020-04-12 19:56:08 -0300

    adjustment to register controller

--- src/main/java/com/gobs/oauthservice/controllers/SignUpController.java
+++ src/main/java/com/gobs/oauthservice/controllers/SignUpController.java
@@ -1,42 +1,25 @@
 package com.gobs.oauthservice.controllers;
 
-import com.gobs.oauthservice.domain.entities.User;
-import com.gobs.oauthservice.domain.exceptions.user.UserAlreadyRegisteredException;
+import javax.inject.Inject;
+
+import com.gobs.oauthservice.domain.dtos.UserDTO;
 import com.gobs.oauthservice.services.signup.SignUpService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SignUpController {
 
-    // Logger logger = LoggerFactory.getLogger(SignUpController.class);
-
+    @Inject
     SignUpService service;
 
     @PostMapping("/register")
-    public String signup(@RequestBody User user, Model model) throws Exception {
-
-        try {
-            System.out.println("Registering new user...");
-            user = service.register(user);
-            System.out.println("User is registered...");
-
-            // if no exceptions was thrown adds a success attribute.
-            model.addAttribute("success", "true");
-
-        } catch (UserAlreadyRegisteredException ex) {
-            model.addAttribute("error_message", "Um usuário com este endereço de email já está cadastrado!");
-        } catch (Exception ex) {
-            model.addAttribute("error_message", "Oops, algo deu errado!");
-        }
-
-        return "signup.html";
+    public ResponseEntity<?> signup(@RequestBody UserDTO userDTO, Model model) throws Exception {
+        return ResponseEntity.ok(service.register(userDTO));
     }
 
 }
--- src/main/java/com/gobs/oauthservice/services/signup/SignUpService.java
+++ src/main/java/com/gobs/oauthservice/services/signup/SignUpService.java
@@ -1,21 +1,23 @@
 package com.gobs.oauthservice.services.signup;
 
+import com.gobs.oauthservice.domain.dtos.UserDTO;
 import com.gobs.oauthservice.domain.entities.User;
+import com.gobs.oauthservice.domain.mappers.users.UserMapper;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpService {
 
-    public User register(User user) throws Exception {
-        // transform to entity.
-        // ...
+    public UserDTO register(UserDTO userDTO) throws Exception {
+        // transform DTO to Entity
+        User user = UserMapper.toEntity(userDTO);
 
         // validating...
 
         // saving...
         
-        return user; // transform to dto.
+        return UserMapper.toDto(user); // transform to dto.
     }
 
 }
